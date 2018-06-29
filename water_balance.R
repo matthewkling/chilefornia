8521d5c290db25c1886754f4b8661aa3859f58b6
Matthew Kling <mattkling@gmail.com>
2018-06-29 14:37:35 -0700

    cleanup and commenting

--- water_balance.R
+++ water_balance.R
@@ -1,4 +1,16 @@
 
+# This script includes a set of functions used to calculate annual
+# hydrologic variables from monthly temperature and precipitation data.
+
+# References:
+#     Wang et al 2012 -- ClimateWNA -- Journal of Applied Meteorology and Climatology
+#     Shuttleworth 1993 -- Evaporation (chapter 4 in Handbook of Hydrology)
+#     Hargreaves and Samani 1985 -- Reference Crop Evaporation from Ambient Air Temperature
+
+# Matthew Kling, June 2018
+
+
+
 # Extraterrestrial solar radiation, per Shuttleworth 1993
 ETSR <- function(psi, # latitude, in degrees
                  J){ # julian day
@@ -25,18 +37,33 @@ ETSR <- function(psi, # latitude, in degrees
       return(S0)
 }
 
+
 # mean S0 for a month of the year
 monthly_S0 <- function(month, latitude){
       
       # julian days for target month
-      jdays <- which(jmonths==month)
+      #j <- as.Date(0:364, format="%j", origin=as.Date("2018-01-01"))
+      #j <- as.integer(substr(as.character(j), 6, 7))
+      j <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+             2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 
+             3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
+             4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+             5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 
+             6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 
+             7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 
+             8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 
+             9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 
+             10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 
+             11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 
+             12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12)
+      jdays <- which(j==month)
       
       # average ETSR across all days of the month
       mean(ETSR(latitude, jdays))
 }
 
-# Hargreaves equation for evapotranspiration
-# per Hargreaves & Samani 1985
+
+# Hargreaves equation for evapotranspiration, per Hargreaves & Samani 1985
 # (Shuttleworth 1993 seems to incorrectly omit the exponent)
 hargreaves <- function(S0, tmean, tmin, tmax){
       
@@ -44,17 +71,22 @@ hargreaves <- function(S0, tmean, tmin, tmax){
       0.0023 * S0 * (tmax - tmin)^.5 * (tmean + 17.8)
 }
 
+
 # calculate monthly and annual water balance variables for one site
 hydro <- function(latitude, # integer
                   ppt, tmean, tmax, tmin){ # vectors of length 12
-      #browser()
+      
       if(is.na(tmean[1])) return(rep(NA, 5))
+      
+      # [note: Wang et al 2012 page 21 use a latitude correction;
+      # this note is a placeholder for that calculation if we decide to use it]
       
       # get S0 values for all 12 months
       S0 <- sapply(1:12, monthly_S0, latitude=latitude)
       
       # monthly water balance variables
-      pet <- hargreaves(S0, tmean, tmin, tmax) * dayspermonth
+      pet <- hargreaves(S0, tmean, tmin, tmax) * 
+            c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
       pet[tmean<0] <- 0 # per Wang et al 2012
       aet <- pmin(pet, ppt)
       cmd <- pmax(0, pet - ppt)
@@ -64,13 +96,11 @@ hydro <- function(latitude, # integer
       return(c(PPT=sum(ppt), PET=sum(pet), AET=sum(aet), CWD=sum(cmd), RAR=sum(rr)))
 }
 
+
 # calculate annual water balance variables for a raster stack
-water_balance <- function(rasters, temp_scalar, ncores){ #stack of 49 rasters: ppt1-12, tmean1-12, tmax1-12, tmin1-12, latitude
-      
-      # sequence of months corresponding to julian dates for a non-leap year
-      jmonths <- as.Date(0:364, format="%j", origin=as.Date("2018-01-01"))
-      jmonths <- as.integer(substr(as.character(jmonths), 6, 7))
-      dayspermonth <- as.vector(table(jmonths))
+water_balance <- function(rasters, # stack of 49 rasters: ppt1-12, tmean1-12, tmax1-12, tmin1-12, latitude
+                          temp_scalar=1, # multiplier to convert input temperatures to deg C
+                          ncores=1){ # number of computing cores to use for parallel processing
       
       # compute annual water balance variables
       w <- function(x, ...) hydro(latitude=x[49], 
@@ -78,12 +108,11 @@ water_balance <- function(rasters, temp_scalar, ncores){ #stack of 49 rasters: p
                                   tmean=x[13:24],
                                   tmax=x[25:36],
                                   tmin=x[37:48])
-      if(temp_scalar) w <- function(x, ...) hydro(latitude=x[49], 
-                                                  ppt=x[1:12], 
-                                                  tmean=x[13:24]/10,
-                                                  tmax=x[25:36]/10,
-                                                  tmin=x[37:48]/10)
-      
+      if(temp_scalar != 1) w <- function(x, ...) hydro(latitude=x[49], 
+                                                       ppt=x[1:12], 
+                                                       tmean=x[13:24] * temp_scalar,
+                                                       tmax=x[25:36] * temp_scalar,
+                                                       tmin=x[37:48] * temp_scalar)
       
       beginCluster(ncores, type="SOCK")
       wb <- clusterR(rasters, calc, args=list(fun=w), 
@@ -96,28 +125,23 @@ water_balance <- function(rasters, temp_scalar, ncores){ #stack of 49 rasters: p
 }
 
 
-latitude <- function(template, 
-                     enforce_latlong=TRUE){ # leave as T unless rasters are already in lat-long projection
-      
+# generate a raster layer with call values representing degrees latitude
+latitude <- function(template, # file path to a raster layer
+                     already_latlong=FALSE){ # leave as F unless rasters are already in lat-long projection
       
       lat <- raster(template)
-      lat <- lat * 1
-      lat[] <- coordinates(lat)[,2]
-      return(lat)
+      lat <- lat * 1 # force into RAM
+      coords <- coordinates(lat)
       
-      # FIXME: implement reprojection of non-lat-long coordinates
-      # (code below is deprecated version that is not memory-safe)
-      
-      # create latitude raster
-      coords <- as.data.frame(coordinates(raster))
-      if(enforce_latlong){
+      if(!already_latlong){
+            coords <- as.data.frame(coords)
             coordinates(coords) <- c("x", "y")
-            crs(coords) <- crs(rasters)
+            crs(coords) <- crs(lat)
             coords <- spTransform(coords, crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
             coords <- coordinates(coords)
       }
-      lat <- raster
-      lat[] <- coords[,2] # note: Wang et al 2012 page 21 use a latitude correction that we could decide to implement here
+      
+      lat[] <- coords[,2]
       return(lat)
 }
 
